c16ab60e34174d3463ec7e9a3cd8d230745b3aa7
Karol Krawczyk <karol.krawczyk1@edu.uekat.pl>
2021-04-15 21:29:08 +0200

    created main file

---
+++ main.py
@@ -0,0 +1,5 @@
+from src.encryption import Encrypt
+from src.decryption import Decrypt
+
+if __name__ == '__main__':
+    pass
--- src/decryption.py
+++ src/decryption.py
@@ -1,61 +1,70 @@
 import random
 import enchant
 from src.encryption import Encrypt
+import re
 
-ALPHABET = 'abcdefghijklmnopqrstuwxyz'
 
-
-def is_english(text):
+def is_english(text:str) -> bool:
     """
     Checks if given word is a correct word in english.
-    :param word: word to check
-    :return: bool, True if given word is a correct english word
+    :param text: text to check
+    :return: returns True if given text consists of only valid english words
     """
+
+    # initialize the english dictionary
     english_dict = enchant.Dict('en_US')
 
     for word in text.split():
-        if not english_dict.check(word):  # if word is not english
+        if not english_dict.check(word):  # if word is not in dictionary
             return False
     return True
 
 
 class Decrypt:
-    def __init__(self, key):
+    def __init__(self, key:int):
+        """
+        Class used to decrypt previously encoded message, with the same key that was used to encrypt it.
+        :param key: key used in encrypting the message
+        """
         self.key = key
 
-    def decrypt_letter(self, letter, secret_value):
+    def decrypt_letter(self, letter: str, secret_value: int) -> str:
         """
+        Giving the different secret value used to encrypt the message, function returns the letter that matches
+        the output of the encrypting algorithm. For example if letter 'd' before encryption was 'm', function
+        returns 'm'.
 
-        :param letter:
-        :param secret_value:
+        :param letter: letter to decrypt
+        :param secret_value: secret value used for encrypting the text.
+                            Function gives the correct output only if those values match.
         :return:
         """
-        indexes = list(range(len(ALPHABET)))
+        indexes = list(range(len(Encrypt.ALPHABET)))
         for index in indexes:
-            if ((index + secret_value) * self.key) % len(ALPHABET) == ALPHABET.index(letter):
-                return ALPHABET[index]
+            if ((index + secret_value) * self.key) % len(Encrypt.ALPHABET) == Encrypt.ALPHABET.index(letter):
+                return Encrypt.ALPHABET[index]
 
-    def decrypt_text(self, text):
+    def decrypt_text(self, text: str) -> str:
         """
-
+        De
         :param text:
         :return:
         """
-        # odd numbers from 3 to 21
-        secret_values = list(range(2, 25))
-        new_text = ''
 
-        for sv in secret_values:
-            new_text = ''.join(self.decrypt_letter(letter, sv) if letter in ALPHABET else letter for letter in text.lower())
+        for sv in range(2, 26):
+            decrypted_text = ''.join(self.decrypt_letter(letter, sv) if letter in ALPHABET else letter for letter in text.lower())
+            # text with removed punctuation
+            decrypted_text_clear = re.sub(r'[,!?.]', '', decrypted_text)
 
-            if is_english(new_text):
-                return new_text
+            # if whole text consists of valid english words, then it's correctly decrypted message
+            if is_english(decrypted_text_clear):
+                return decrypted_text
 
 
-text = 'good evening my dear friends whom I love with all my heart'
-e = Encrypt(23)
+text = 'your stuff, is broken. hello carpenter'
+e = Encrypt(key = 4)
 encrypted = e.encrypt_text(text)
 print('Encrypted word:', encrypted)
 
-d = Decrypt(23)
+d = Decrypt(key = 4)
 print('Decrypted word:', d.decrypt_text(encrypted))
